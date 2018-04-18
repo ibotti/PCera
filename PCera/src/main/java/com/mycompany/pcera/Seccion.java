ceb2a9ecd311c46124936f161e16fc65c1ba2a7c
Juan Antonio <Juan Antonio@LAPTOP-MEO5DRKE>
2018-04-18 19:48:39 +0200

    Creación de la clase

---
+++ PCera/src/main/java/com/mycompany/pcera/Seccion.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pcera;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Juan Antonio
+ */
+@Entity
+public class Seccion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 100)
+    private String nombre;
+    @OneToMany(mappedBy="seccion")
+    private List<Promesa> promesas = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Promesa> getPromesas() {
+        return promesas;
+    }
+
+    public void setPromesas(List<Promesa> promesas) {
+        this.promesas = promesas;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Seccion)) {
+            return false;
+        }
+        Seccion other = (Seccion) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.pcera.Seccion[ id=" + id + " ]";
+    }
+
+}
